737dd76b6725cfe4e9b429e01f1b85f730db3d7b
Manas Panigrahi <manaskumarpanigrahi62gmail.com>
2023-09-24 05:03:19 +0530

    all react code

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <>
+        <header className='mt-2'>
+          <nav>
+            <div className='d-flex flex-row align-items-center '>
+              <h1 className='mx-auto'>Syook </h1>
+            </div>
+          </nav>
+        </header>
+      </>
+    )
+}
+
+export default Header
